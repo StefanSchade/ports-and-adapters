ff66336f1ff9a2493a44ad091f7b2f9aa78b22ef
Dr. Stefan Schade <stefan.schade@msg-gillardon.de>
2020-08-13 14:22:23 +0200

    initial commit. Closes #1

---
+++ src/main/java/com/example/portsandadapters/PortsAndAdaptersApplication.java
@@ -0,0 +1,13 @@
+package com.example.portsandadapters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortsAndAdaptersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortsAndAdaptersApplication.class, args);
+    }
+
+}
