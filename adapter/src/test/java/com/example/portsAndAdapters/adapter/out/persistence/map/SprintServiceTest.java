286db3d76a7434d07f843e89190e8ec36306e975
Dr. Stefan Schade <stefan.schade@msg-gillardon.de>
2020-08-18 09:21:54 +0200

    rework pom. closes #9

---
+++ adapter/src/test/java/com/example/portsAndAdapters/adapter/out/persistence/map/SprintServiceTest.java
@@ -0,0 +1,33 @@
+package com.example.portsAndAdapters.adapter.out.persistence.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SprintServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByID() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteByID() {
+    }
+}
