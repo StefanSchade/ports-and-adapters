138f2d58d4a6c89b435c86c07a67af7cd29aff73
Dr. Stefan Schade <stefan.schade@msg-gillardon.de>
2020-08-21 14:56:14 +0200

    Overdoing it with Abstraction

--- application/src/main/java/com/example/portsandadapters/application/domainstories/command/sprint/commitbacklogitem/HandlerSprintCommitBacklogItem.java
+++ application/src/main/java/com/example/portsandadapters/application/domainstories/command/sprint/commitbacklogitem/HandlerSprintCommitBacklogItem.java
@@ -1,24 +1,20 @@
 package com.example.portsandadapters.application.domainstories.command.sprint.commitbacklogitem;
 
 import com.example.portsandadapters.application.port.in.command.HandlerAbstraction;
-import com.example.portsandadapters.application.port.out.persistence.PersistenceAbstraction;
 import com.example.portsandadapters.domain.model.aggregate.Sprint.Sprint;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
-@RequiredArgsConstructor
-public class HandlerSprintCommitBacklogItem implements HandlerAbstraction<CommandSprintCommitBacklogItem> {
-
-    @NonNull private final PersistenceAbstraction<Sprint, Long> aggregateRepository;
+@SuperBuilder
+public class HandlerSprintCommitBacklogItem extends HandlerAbstraction<Sprint, CommandSprintCommitBacklogItem> {
 
     @Override
     public void handle(CommandSprintCommitBacklogItem command) {
 
-        Sprint sprint = aggregateRepository.findByID(command.getSprintId());
+        Sprint sprint = this.getAggregateRepository().findByID(command.getSprintId());
 
   //      backlogItem.commitToSprint(sprint);
 
-        aggregateRepository.save(command.getSprintId(), sprint);
+        this.getAggregateRepository().save(command.getSprintId(), sprint);
 
     }
 }
--- application/src/main/java/com/example/portsandadapters/application/port/in/command/HandlerAbstraction.java
+++ application/src/main/java/com/example/portsandadapters/application/port/in/command/HandlerAbstraction.java
@@ -1,7 +1,20 @@
 package com.example.portsandadapters.application.port.in.command;
 
-public interface HandlerAbstraction<T extends CommandAbstraction> {
+import com.example.portsandadapters.application.port.out.persistence.PersistenceAbstraction;
+import com.example.portsandadapters.domain.model.base.AggregateRoot;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-    public void handle(T commandInterface);
+@SuperBuilder
+@Getter
+@Setter(AccessLevel.PRIVATE)
+public abstract class HandlerAbstraction<A extends AggregateRoot, T extends CommandAbstraction> {
+
+    @NonNull private final PersistenceAbstraction<A, Long> aggregateRepository;
+
+    public abstract void handle(T commandInterface);
 
 }
