b4be59db1e7c89386f0874340c28030b0353dd80
Dr. Stefan Schade <stefan.schade@msg-gillardon.de>
2020-08-21 14:43:50 +0200

    Refactor

--- application/src/main/java/com/example/portsandadapters/application/domainstories/command/sprint/commitbacklogitem/HandlerSprintCommitBacklogItem.java
+++ application/src/main/java/com/example/portsandadapters/application/domainstories/command/sprint/commitbacklogitem/HandlerSprintCommitBacklogItem.java
@@ -2,7 +2,6 @@ package com.example.portsandadapters.application.domainstories.command.sprint.co
 
 import com.example.portsandadapters.application.port.in.command.HandlerAbstraction;
 import com.example.portsandadapters.application.port.out.persistence.PersistenceAbstraction;
-import com.example.portsandadapters.domain.model.aggregate.Sprint.BacklogItem;
 import com.example.portsandadapters.domain.model.aggregate.Sprint.Sprint;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -10,16 +9,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class HandlerSprintCommitBacklogItem implements HandlerAbstraction<CommandSprintCommitBacklogItem> {
 
-    @NonNull private final PersistenceAbstraction<Sprint, Long> sprintRepo;
+    @NonNull private final PersistenceAbstraction<Sprint, Long> aggregateRepository;
 
     @Override
     public void handle(CommandSprintCommitBacklogItem command) {
 
-        Sprint sprint = sprintRepo.findByID(command.getSprintId());
+        Sprint sprint = aggregateRepository.findByID(command.getSprintId());
 
   //      backlogItem.commitToSprint(sprint);
 
-        sprintRepo.save(command.getSprintId(), sprint);
+        aggregateRepository.save(command.getSprintId(), sprint);
 
     }
 }
