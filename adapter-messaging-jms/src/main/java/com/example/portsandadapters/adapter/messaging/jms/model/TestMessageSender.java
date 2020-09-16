a30914e108d368da0b9131a2a5a559635d144e60
Andreas Konrad <andreas.konrad@msg-gillardon.de>
2020-09-16 06:40:40 +0200

    Replaced System.out with log.info in messaging classes.

--- adapter-messaging-jms/src/main/java/com/example/portsandadapters/adapter/messaging/jms/model/TestMessageSender.java
+++ adapter-messaging-jms/src/main/java/com/example/portsandadapters/adapter/messaging/jms/model/TestMessageSender.java
@@ -1,17 +1,19 @@
 package com.example.portsandadapters.adapter.messaging.jms.model;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.annotation.Scheduled;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.UUID;
 
+@Slf4j
 @RequiredArgsConstructor
 public class TestMessageSender {
 
     private final JmsTemplate jmsTemplate;
 
-    @Scheduled(fixedRate = 2000)
+    @Scheduled(fixedRate = 5000)
     public void sendMessage() {
 
         HelloWorldMessage message = HelloWorldMessage
@@ -22,9 +24,6 @@ public class TestMessageSender {
 
         jmsTemplate.convertAndSend(HelloWorldMessage.TEST_QUEUE, message);
 
-        System.out.println("Message Sent!");
-
+        log.info("Message Sent!");
     }
-
-
 }
